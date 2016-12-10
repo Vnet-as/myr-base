0a54aacc489b2cba851b8f066beb0b15b341d013
Michal Kuffa <michal.kuffa@gmail.com>
2016-12-10 23:29:08 +0100

    Temp tests fix
    Signed-off-by: Michal Kuffa <michal.kuffa@gmail.com>

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,13 +1,10 @@
-import mock
-import pytest
-from pytest_mock import mocker
-
 from myr.base.app import (
     MyrApp,
     announce_task,
     get_function_spec,
     ENV
 )
+
 
 class TestApp:
 
@@ -17,24 +14,37 @@ class TestApp:
         assert 'announce_task' in app.conf.beat_schedule
 
     def test_announcing_no_tasks(self, mocker):
-        app = MyrApp()
-        mocker.patch.object(app, 'send_task')
-        announce_task(app)
-        app.send_task.assert_called_with(ENV.get('MYR_ANNOUNCE_TASK'),
-                args=[{}], queue=ENV.get('MYR_ANNOUNCE_QUEUE'))
+
+        class Task:
+            app = MyrApp()
+
+        mocker.patch.object(Task.app, 'send_task')
+
+        announce_task(Task)
+        Task.app.send_task.assert_called_with(
+            ENV.get('MYR_ANNOUNCE_TASK'),
+            args=[{}],
+            queue=ENV.get('MYR_ANNOUNCE_QUEUE'))
 
     def test_announcing_user_tasks(self, mocker):
-        app = MyrApp()
-        mocker.patch.object(app, 'send_task')
-        @app.task
+
+        class Task:
+            app = MyrApp()
+
+        mocker.patch.object(Task.app, 'send_task')
+
+        @Task.app.task
         def test_task(a1, a2='2'):
             pass
-        announce_task(app)
+
+        announce_task(Task)
         tasks_spec = {
             test_task.name: {
                 'signature': get_function_spec(test_task.run)._asdict(),
                 'routing': {'queue': 'celery'}
             }
         }
-        app.send_task.assert_called_with(ENV.get('MYR_ANNOUNCE_TASK'),
-                args=[tasks_spec], queue=ENV.get('MYR_ANNOUNCE_QUEUE'))
+        Task.app.send_task.assert_called_with(
+            ENV.get('MYR_ANNOUNCE_TASK'),
+            args=[tasks_spec],
+            queue=ENV.get('MYR_ANNOUNCE_QUEUE'))
